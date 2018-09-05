9ef76f07d697e966781fd2f7ea9147d3396b2df9
HappyCcccc <tuf99286@temple.edu>
2018-09-05 14:33:43 -0400

    Add files via upload

---
+++ ClusterCost1.m
@@ -0,0 +1,413 @@
+global Z
+
+% Z=[0.751904250043374	0.810497223982770
+% 0.672506978601652	0.816657849309698
+% 0.570078184459027	0.891591737403496
+% 0.703908898750641	0.931440194453365
+% 0.650506955295838	0.831888208772236
+% 0.723038203808938	0.986581194054759
+% 0.526004261692982	0.830163546854113
+% 0.751195790371223	0.854822992977871
+% 0.543197736539153	0.668212170958139
+% 0.691248610930399	0.949930465609956
+% 0.534191642700814	0.796419436662759
+% 0.673335488724140	0.844728340078672
+% 0.534414100077343	0.939361772604052
+% 0.715532593188686	0.953036376521660
+% 0.609087383360289	0.938531486720960
+% 0.862729708978997	0.955373668050092
+% 0.740527902072256	0.869843337342491
+% 0.719245930560400	0.787901802565386
+% 0.877036716908924	0.848169184333941
+% 0.619478285414474	0.921702939485518
+% 0.176016953731159	0.0654308805478222
+% 0.305346973995728	0.0470546469296427
+% 0.147897453152010	0.211773271661001
+% 0.182936312403649	0.131389036550744
+% 0.165758987975318	0.137203118928841
+% 0.304381643507140	0.162880344495757
+% 0.0681598245517225	0.0791065180254412
+% 0.0458948615001826	0.0497967349572590
+% 0.200726720880622	0.00859263504366231
+% 0.188758578826755	0.0852447141477599
+% 0.146471210696866	0.0767187790522415
+% 0.126557136285210	0.102699670720224
+% 0.0303948614256457	0.0276159855932840
+% 0.232366949448875	0.0941736664561296
+% 0.211539908838982	0.0168877873038932
+% 0.0421969652305077	0.121141122258030
+% 0.0750621165219949	0.0840481088853967
+% 0.171915571014992	0.207910944506797
+% 0.187006215793510	0.302349035017743
+% 0.192846973252931	0.0570573918840905
+% 0.114697473455972	0.886999064282995
+% 0.252513595443641	0.716444358447671
+% 0.114240306366950	0.739881290006984
+% 0.192756441912143	0.827528469017887
+% 0.267389267159990	0.903381857220123
+% 0.349073454293976	0.720622257288560
+% 0.0559445698125965	0.682563851396165
+% 0.0420814559565530	0.670109526129127
+% 0.0942043871451027	0.784406889679285
+% 0.302759829065394	0.921825680767188
+% 0.122041015196762	0.774051018062156
+% 0.256544909439677	0.911327609948200
+% 0.157708703809870	0.753961193164588
+% 0.0277136464165587	0.652150586309082
+% 0.119738462219728	0.804561941867491
+% 0.207980915679688	0.852574190959789
+% 0.262605553003749	0.884909566343923
+% 0.164839114805510	0.766055746548426
+% 0.0109290546977406	0.687710925430381
+% 0.124019767681202	0.762893740856269];
+
+Z=[0.6524    0.9569
+    0.7392    0.9656
+    0.6891    0.9426
+    0.7278    0.7887
+    0.7039    0.9930
+    0.6035    0.8954
+    0.7345    0.9251
+    0.8200    0.9890
+    0.7237    0.8970
+    0.8431    0.9596
+    0.7185    0.9819
+    0.5517    0.8160
+    0.6290    0.9434
+    0.5854    0.8642
+    0.6525    0.8761
+    0.6673    0.9207
+    0.6293    0.8141
+    0.6962    0.8667
+    0.5105    0.7426
+    0.5384    0.8079
+    0.0237    0.0164
+    0.1646    0.0236
+    0.1036    0.0656
+    0.2766    0.1170
+    0.1134    0.1112
+    0.0740    0.0120
+    0.2682    0.1791
+    0.0147    0.0040
+    0.0821    0.2005
+    0.2034    0.1145
+    0.0691    0.0798
+    0.1517    0.0198
+    0.1871    0.0578
+    0.0174    0.0454
+    0.0273    0.1123
+    0.2046    0.1195
+    0.1894    0.0924
+    0.1248    0.1939
+    0.0580    0.0287
+    0.1742    0.0450
+    0.2484    0.8328
+    0.2099    0.7920
+    0.2708    0.8603
+    0.0760    0.7971
+    0.0890    0.6454
+    0.0991    0.6526
+    0.1853    0.7726
+    0.1186    0.7163
+    0.0065    0.7512
+    0.1664    0.9850
+    0.0328    0.6970
+    0.1485    0.7342
+    0.0537    0.8562
+    0.1676    0.8464
+    0.1949    0.6703
+    0.1895    0.8795
+    0.1112    0.6866
+    0.1438    0.6953
+    0.1051    0.7323
+    0.0917    0.6668
+    0.6821    0.6524
+    0.7781    0.4464
+    0.9246    0.4079
+    0.9894    0.3337
+    0.7741    0.4671
+    0.8996    0.5807
+    0.8261    0.6133
+    0.7507    0.7254
+    0.7232    0.3027
+    0.9678    0.3987
+    0.8247    0.5794
+    0.6188    0.5518
+    0.8384    0.5990
+    0.7251    0.5907
+    0.8717    0.5324
+    0.8291    0.4937
+    0.7655    0.5649
+    0.7291    0.6953
+    0.9719    0.4698
+    0.8716    0.3853
+    0.3602    0.8376
+    0.4950    0.6217
+    0.3891    0.6270
+    0.5583    0.6473
+    0.4629    0.4990
+    0.7135    0.4021
+    0.4867    0.4199
+    0.4778    0.3121
+    0.4426    0.4417
+    0.6473    0.3921
+    0.5267    0.4210
+    0.4692    0.4958
+    0.4561    0.2002
+    0.3814    0.5186
+    0.6968    0.4812
+    0.6354    0.2968
+    0.4760    0.4955
+    0.5697    0.5876
+    0.3290    0.6963
+    0.5466    0.3900];
+global k
+
+%scatter(Z(:,1), Z(:,2));
+
+fileID1 = fopen('km_re_cost.txt','w');
+fileID2 = fopen('km_re_finalpoint.txt','w');
+fileID3 = fopen('km_re_diff.txt','w');
+
+%for ii = 1:1000
+    
+    
+% opts = statset('Display','final');
+% [idx,C2,sum0] = kmeans(Z,k,'Replicates',1,'Options',opts);
+% %kmcost = sum(sum0);
+% kmcost = sum(sum0);
+% fprintf(fileID1,'kcost=%f\n',kmcost);
+% fprintf(fileID2, 'c2=%f\n',C2);
+oldVn =0;
+oldMn =0;
+count = 0;
+tic
+d = 10;
+n = 20000;
+Corner = zeros(1,d);% matrix for corner node
+Width = zeros(1,d);% matrix for width and length
+Center = zeros(1,d);% matrix for center node
+area = ones(1,1); % matrix for retrangle area
+f = zeros(1,1); % function value for center points
+getrho = zeros(1,1);
+ a = zeros(1,d);
+V = zeros(1,n);
+M = zeros(1,n);
+
+
+for i = 1:d
+    Width(1,i)=1.0;
+    Center(1,i)=0.5;
+end
+
+maxWidth = 0.0;
+maxIndex = 1;
+maxD = 0;
+
+Mn = intmax('int64');
+Vn = intmax('int64');
+Center(maxIndex,:);
+BP = zeros(1,d);
+
+
+for i = 1:n
+flag = 0;    
+maxWidth = 0.0;
+% split the retrangle
+    %decide which axis to split on
+for j = 1:d
+        if(Width(maxIndex,j)>maxWidth)
+            maxWidth = Width(maxIndex,j);
+            maxD = j;
+        end
+end
+
+%update corner node and width/length information for new retrangles
+%NC1 = zeros(d);
+
+Width(maxIndex, maxD)=1/3*maxWidth;   
+Center = Corner + Width/2;
+
+area(maxIndex)=1;
+for j=1:d
+    area(maxIndex) = area(maxIndex)*Width(maxIndex, j);
+end
+
+if(area(maxIndex)<Vn)
+    Vn = area(maxIndex);
+end
+
+a = Center(maxIndex,:);
+f(maxIndex,1)= cluster(a);
+
+if(f(maxIndex,1)<Mn)
+    Mn = f(maxIndex,1);
+    BP = a;
+end
+
+
+%if (area(maxIndex) == Vn || f(maxIndex) == Mn)
+if (oldVn > Vn || oldMn > Mn)
+    flag = 1;
+else 
+    flag = 0;
+end
+
+NC1 = zeros(1,d);
+NC2 = zeros(1,d);
+NW1 = zeros(1,d); 
+NW2 = zeros(1,d);
+NCE1 = zeros(1,d);
+NCE2 = zeros(1,d);
+NA1= ones(1,1);
+NA2= ones(1,1);
+NF1= zeros(1,1);
+NF2 = zeros(1,1);
+%calculate center nodes and retrangle area
+for j = 1:d
+    if (j == maxD)
+        NC1(j) = 1/3*maxWidth + Corner(maxIndex,j);
+        NC2(j) = 2/3*maxWidth + Corner(maxIndex,j);
+        NW1(j) = 1/3*maxWidth;
+        NW2(j) = 1/3*maxWidth;
+    else 
+        NC1(j) = Corner(maxIndex,j);
+        NC2(j) =  Corner(maxIndex,j);
+        NW1(j) = Width(maxIndex,j);
+        NW2(j) =  Width(maxIndex,j);
+    end
+    NA1 = NA1*NW1(1,j);
+    NA2 = NA2*NW2(1,j);
+end
+
+NCE1=NC1+NW1/2;
+NCE2=NC2+NW2/2;
+NF1=cluster(NCE1);
+NF2 = cluster(NCE2);
+
+%get the smallest function value among all vertices
+if(NF1<Mn)
+    Mn = NF1;
+    BP = NCE1;
+    flag = 1;
+    if(NF2<Mn)
+        Mn = NF2;
+        BP = NCE2;
+        flag = 1;
+    end
+end
+
+
+% get the volume of smallest rectangle:travsal all rectangles
+if(NA1<Vn)
+    Vn = NA1;
+    flag = 1;
+    if(NA2<Vn)
+        Vn = NA2;
+        flag = 1;
+    end
+end
+
+%update corner and width information
+Corner = [Corner;NC1;NC2]; 
+Width = [Width;NW1;NW2];
+Center = [Center;NCE1;NCE2];
+area = [area;NA1;NA2];
+f = [f;NF1;NF2];
+
+[m,d] = size(Corner);
+%  Vn = min(area);
+%   Mn = min(f);
+
+
+% get GVn
+%GVn = d*(Vn*log(1/Vn)).^(d/2);
+%GVn = d*(Vn*log(1/Vn)).^(d/4);
+GVn = 4*(Vn^(1/2))*(d*log(3/(Vn^(2/d))))^(d/4);
+
+%GVn = 4.0*(Vn^(r/d))*(0.5*d*log(evalCount+2.0))^(r/d);
+
+%choose the retrangle which have the largest Rho
+%rho = intmin;
+%figure (1)
+%axis equal 
+%axis ([0 1 0 1])
+%hold on
+
+
+if (i==1||flag)
+%if (i==1||V(i)~=V(i-1) || M(i)~= M(i-1))
+    for j = 1 : m
+    % a= Center(j,:);
+    % b = Center(j,:);
+     %getrho(j) = area(j,1).^(2/d)/(funct(a,d)-M(1,i)+d*GVn);
+    %   getrho(j) = area(j,1).^(2/d)/(f(j)-Mn+GVn); %better for 2d raf
+  getrho(j) =area(j,1).^(2/d)*log(1.01+(f(j)-Mn)/GVn).^(2/d)/(f(j)-Mn+d*GVn);
+  % getrho(j) = area(j,1)^(r/d)/(f(j)-Mn+GVn);
+   
+    end
+     
+        [tmp, ind] = sort(getrho,'descend');
+        count = count +1;
+        Center = Center(ind,:);
+        Corner = Corner(ind,:);
+        Width = Width(ind,:);
+        f = f(ind);
+        area = area(ind);
+
+maxIndex = 1;
+   
+else
+    maxIndex = maxIndex+1;
+end
+
+
+
+ Center(maxIndex, :);
+ %plot(Center(:,1), Center(:,2),'.');
+% scatter3(Center(:,1),Center(:,2),Center(:,3),'*');
+
+% z = f(:,1);
+% x = Center(:,1);
+% y = Center(:,2);
+
+oldVn = Vn;
+oldMn = Mn;
+
+end
+toc
+count
+Mn;
+BP;
+cp = zeros(1,1); %cp is the start point of the k-means 
+j=1;
+for i=1:k
+    cp(i,1)=BP(j);
+    cp(i,2)=BP(j+1);
+    j=j+1;
+end
+
+
+% opts = statset('Display','final');%use k-means divide the dataset into k clusters
+% [idx,C,sum1] = kmeans(Z,k,'Distance','sqeuclidean',...
+%     'Replicates',1,'Start',cp,'Options',opts);
+% %cost = sum(sum1)
+% cost = sum(sum1);
+% 
+% fprintf(fileID1,'cost=%f\n',cost);
+% fprintf(fileID2, 'c=%f\n',C);
+
+% if(cost<kmcost)
+%     diff = kmcost - cost;
+%     fprintf(fileID3, 'diff = %f\n',diff);
+% end
+
+%fprintf(fileID3, 'diff = %f\n',diff);
+%end
+
+% fclose(fileID1);
+% fclose(fileID2);
+
+
+
+
